d3ba1978913101a9c7a1e7eca63efd37b8c15afc
kolibrik666 <58887215+kolibrik666@users.noreply.github.com>
2024-03-03 20:11:12 +0100

    UI setup

---
+++ Assets/Scripts/GameSetupData.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+public class GameSetupData : MonoBehaviour
+{
+    public int SanityValue;
+    public int SaturationValue;
+    public int SurvivedDays;
+    public int BatteryValue;
+}
---
+++ Assets/Scripts/SurvivalManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SurvivalManager : MonoBehaviour
+{
+    [SerializeField] Slider _hungerBarSlider;
+    [SerializeField] Slider _sanityBarSlider;
+    [SerializeField] GameSetupData _gameSetupData;
+    int _hungerValue;
+    int _sanityValue;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
